35e08d78d09d23298b7cc0e67a58288ed07e131d
Mario <babayagapa@gmail.com>
2026-05-15 13:42:32 +0200

    com

--- pms-frontend/src/main.jsx
+++ pms-frontend/src/main.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import './index.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
